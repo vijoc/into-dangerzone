393dbfa1d3a7a176f01cfc6065d6bc7119419eb3
Ville Kukkonen <kukkonen.ville@gmail.com>
2014-09-26 21:43:09 +0300

    Rudimentary implementation for scope and spectrum.

--- IntoDangerzone/src/IntoDangerzone.java
+++ IntoDangerzone/src/IntoDangerzone.java
@@ -1,30 +1,49 @@
 import processing.core.*;
 import ddf.minim.*;
+import ddf.minim.analysis.FFT;
 
 @SuppressWarnings("serial")
 public class IntoDangerzone extends PApplet {
 
 	Minim minim;
-	AudioPlayer player;
+	AudioPlayer song;
 	AudioInput input;
-	
+	FFT fft;
+
 	public void setup() {
-		size(200, 200);
+		size(700, 200);
 		background(0);
+		
 		minim = new Minim(this);
-		player = minim.loadFile("test.mp3");
-		input = minim.getLineIn();
-		player.play();
+		
+		song = minim.loadFile("test.mp3");
+		song.play();
+		
+		fft = new FFT(song.bufferSize(), song.sampleRate());
 	}
 
 	public void draw() {
+		background(0);
+		
+		fft.forward(song.mix);
+
+		stroke(255, 0, 0, 128);
+		
+		for (int i = 0; i < fft.specSize(); i++) {
+			line(i, height, i, height - fft.getBand(i) * 4);
+		}
+
 		stroke(255);
-		if (mousePressed) {
-			line(mouseX, mouseY, pmouseX, pmouseY);
+
+		for (int i = 0; i < width - 1; i++) {
+			line(i, 50 + song.left.get(i) * 50, i + 1,
+					50 + song.left.get(i + 1) * 50);
+			line(i, 150 + song.right.get(i) * 50, i + 1,
+					150 + song.right.get(i + 1) * 50);
 		}
 	}
 
 	public static void main(String args[]) {
-		PApplet.main(new String[] { "--present", "MyProcessingSketch" });
+		PApplet.main(new String[] { "--present", "IntoDangerzone" });
 	}
 }
