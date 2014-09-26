06a62e222302b013829b3f03d9647745eb9172ed
Ville Kukkonen <kukkonen.ville@gmail.com>
2014-09-26 22:57:26 +0300

    Fool around with 3D. Yay.

--- IntoDangerzone/src/IntoDangerzone.java
+++ IntoDangerzone/src/IntoDangerzone.java
@@ -4,11 +4,16 @@ import ddf.minim.analysis.FFT;
 
 @SuppressWarnings("serial")
 public class IntoDangerzone extends PApplet {
-
+	
+	public static final int PARTICLE_COUNT = 50;
+	public static final boolean DRAW_AXES = true;
+	
 	Minim minim;
 	AudioPlayer song;
 	AudioInput input;
 	FFT fft;
+	
+	Particle[] particles = new Particle[PARTICLE_COUNT];
 	
 	// Camera parameters
 	float eyeX, eyeY, eyeZ,
@@ -16,8 +21,14 @@ public class IntoDangerzone extends PApplet {
 		  upX, upY, upZ;
 
 	public void setup() {
-		size(700, 200, P3D);
+		size(1024, 768, P3D);
 		background(0);
+		
+		Vector3D position, velocity;
+		for(int i = 0; i < PARTICLE_COUNT; i++) {
+			//position = new Vector3D();
+			//particles[i] = new Particle()
+		}
 		
 		eyeX = width/2.0f;
 		eyeY = height/2.0f;
@@ -57,11 +68,29 @@ public class IntoDangerzone extends PApplet {
 					150 + song.right.get(i + 1) * 50);
 		}
 		
+		
+		
+		if(DRAW_AXES)
+		{
+			stroke(0,255,0);
+			fill(0,255,0);
+			line(-1000, 0, 0, 1000, 0, 0);
+			line(0, -1000, 0, 0, 1000, 0);
+			line(0, 0, -1000, 0, 0, 1000);
+			for(int i = -1000; i < 1001; i+=100)
+			{
+				text(i, i, 0, 0);
+				text(i, 0, i, 0);
+				text(i, 0, 0, i);
+			}
+		}
+		
 		camera(eyeX, eyeY, eyeZ, centerX, centerY, centerZ, upX, upY, upZ);
 		
 		// The following next two lines control 3D effect.
-		eyeZ = eyeZ + 0.1f;
-		eyeX = eyeX + 0.1f;
+		eyeX += 0.1f;
+		eyeY += 0.1f;
+		eyeZ += 0.1f;	
 	}
 
 	public static void main(String args[]) {
