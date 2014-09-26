32f413ceef7aa77df7b561798986e0b1069192b5
Ville Kukkonen <kukkonen.ville@gmail.com>
2014-09-26 20:36:48 +0300

    Reformat the eyesore.

--- IntoDangerzone/src/IntoDangerzone.java
+++ IntoDangerzone/src/IntoDangerzone.java
@@ -3,19 +3,19 @@ import processing.core.*;
 @SuppressWarnings("serial")
 public class IntoDangerzone extends PApplet {
 
-  public void setup() {
-    size(200,200);
-    background(0);
-  }
+	public void setup() {
+		size(200, 200);
+		background(0);
+	}
 
-  public void draw() {
-    stroke(255);
-    if (mousePressed) {
-      line(mouseX,mouseY,pmouseX,pmouseY);
-    }
-  }
-  
-  public static void main(String args[]) {
-	    PApplet.main(new String[] { "--present", "MyProcessingSketch" });
-	  }
+	public void draw() {
+		stroke(255);
+		if (mousePressed) {
+			line(mouseX, mouseY, pmouseX, pmouseY);
+		}
+	}
+
+	public static void main(String args[]) {
+		PApplet.main(new String[] { "--present", "MyProcessingSketch" });
+	}
 }
