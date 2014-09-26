fd68882ce7a4e850ba60194a15b1645e92b402d3
Jukka Eerikäinen <jukka.eerikainen@aalto.fi>
2014-09-26 21:10:51 +0300

    Import minim. Get some song playing.

--- IntoDangerzone/src/IntoDangerzone.java
+++ IntoDangerzone/src/IntoDangerzone.java
@@ -1,11 +1,20 @@
 import processing.core.*;
+import ddf.minim.*;
 
 @SuppressWarnings("serial")
 public class IntoDangerzone extends PApplet {
 
+	Minim minim;
+	AudioPlayer player;
+	AudioInput input;
+	
 	public void setup() {
 		size(200, 200);
 		background(0);
+		minim = new Minim(this);
+		player = minim.loadFile("test.mp3");
+		input = minim.getLineIn();
+		player.play();
 	}
 
 	public void draw() {
