20172887e1bdfdb5e5f64064514b509295564a68
Jukka Eerikäinen <jukka.eerikainen@aalto.fi>
2014-09-26 22:24:44 +0300

    First 3D effect!

--- IntoDangerzone/src/IntoDangerzone.java
+++ IntoDangerzone/src/IntoDangerzone.java
@@ -9,10 +9,25 @@ public class IntoDangerzone extends PApplet {
 	AudioPlayer song;
 	AudioInput input;
 	FFT fft;
+	
+	// Camera parameters
+	float eyeX, eyeY, eyeZ,
+		  centerX, centerY, centerZ,
+		  upX, upY, upZ;
 
 	public void setup() {
 		size(700, 200, P3D);
 		background(0);
+		
+		eyeX = width/2.0f;
+		eyeY = height/2.0f;
+		eyeZ = (float) ((height/2.0f) / Math.tan(PI*30.0 / 180.0f));
+		centerX = width/2.0f;
+		centerY = height/2.0f;
+		centerZ = 0;
+		upX = 0;
+		upY = 1;
+		upZ = 0;
 		
 		minim = new Minim(this);
 		
@@ -41,6 +56,12 @@ public class IntoDangerzone extends PApplet {
 			line(i, 150 + song.right.get(i) * 50, i + 1,
 					150 + song.right.get(i + 1) * 50);
 		}
+		
+		camera(eyeX, eyeY, eyeZ, centerX, centerY, centerZ, upX, upY, upZ);
+		
+		// The following next two lines control 3D effect.
+		eyeZ = eyeZ + 0.1f;
+		eyeX = eyeX + 0.1f;
 	}
 
 	public static void main(String args[]) {
