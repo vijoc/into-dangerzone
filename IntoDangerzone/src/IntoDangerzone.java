879154e015e0042c0b987e5720c89a2396e34f44
Jukka Eerikäinen <jukka.eerikainen@aalto.fi>
2014-09-26 20:25:31 +0300

    Second initial commit. Blow everything up and start anew.

---
+++ IntoDangerzone/src/IntoDangerzone.java
@@ -0,0 +1,20 @@
+import processing.core.*;
+
+public class IntoDangerzone extends PApplet {
+
+  public void setup() {
+    size(200,200);
+    background(0);
+  }
+
+  public void draw() {
+    stroke(255);
+    if (mousePressed) {
+      line(mouseX,mouseY,pmouseX,pmouseY);
+    }
+  }
+  
+  public static void main(String args[]) {
+	    PApplet.main(new String[] { "--present", "MyProcessingSketch" });
+	  }
+}
