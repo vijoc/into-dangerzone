2a800df290beeade7a9ebc5bcb8fbb4fdc4eb21b
Ville Kukkonen <kukkonen.ville@gmail.com>
2014-09-28 17:26:45 +0300

    Refactor Vector3D

--- IntoDangerzone/src/Vector3D.java
+++ IntoDangerzone/src/Vector3D.java
@@ -1,3 +1,7 @@
+/**
+ * Vector3D is an immutable vector in three-dimensional space.
+ *
+ */
 public class Vector3D {
 	private float x, y, z;
 	private float length;
@@ -9,18 +13,34 @@ public class Vector3D {
 		this.length = (float) Math.sqrt(x*x + y*y + z*z);
 	}
 
+	/**
+	 * Return the x-coordinate of this vector.
+	 * @return the x-coordinate of this vector
+	 */
 	public float getX() {
 		return x;
 	}
 
+	/**
+	 * Return the y-coordinate of this vector.
+	 * @return the y-coordinate of this vector
+	 */
 	public float getY() {
 		return y;
 	}
 
+	/**
+	 * Return the z-coordinate of this vector.
+	 * @return the z-coordinate of this vector
+	 */
 	public float getZ() {
 		return z;
 	}
 	
+	/**
+	 * Normalize this vector and return the normalized version.
+	 * @return the normalized version of this vector.
+	 */
 	public Vector3D normalize() {
 		if(length == 0.0) return this;
 		return new Vector3D(x / length, y / length, z / length);
@@ -36,8 +56,8 @@ public class Vector3D {
 	
 	/**
 	 * Adds given vector to this vector and returns the resulting vector.
-	 * @param other
-	 * @return
+	 * @param other the vector to add to this
+	 * @return the resulting vector
 	 */
 	public Vector3D add(Vector3D other) {
 		return new Vector3D(x + other.x, y + other.y, z + other.z);
@@ -45,8 +65,8 @@ public class Vector3D {
 	
 	/**
 	 * Subtracts given vector from this vector and returns the resulting vector.
-	 * @param other
-	 * @return
+	 * @param other the vector to subtract from this
+	 * @return the resulting vector
 	 */
 	public Vector3D subtract(Vector3D other) {
 		return new Vector3D(x - other.x, y - other.y, z - other.z);
@@ -54,8 +74,8 @@ public class Vector3D {
 	
 	/**
 	 * Divides this vector by given scalar and returns the resulting vector.
-	 * @param scalar
-	 * @return
+	 * @param scalar the scalar to divide this vector by
+	 * @return the resulting vector
 	 */
 	public Vector3D scalarDivision(float scalar) {
 		return new Vector3D(x / scalar, y / scalar, z / scalar);
@@ -63,22 +83,19 @@ public class Vector3D {
 	
 	/**
 	 * Multiplies this vector by given scalar and returns the resulting vector.
-	 * @param scalar
-	 * @return
+	 * @param scalar the scalar to multiply this vector by
+	 * @return the resulting vector
 	 */
 	public Vector3D scalarMultiplication(float scalar) {
 		return new Vector3D(x * scalar, y * scalar, z * scalar);
 	}
 	
 	/**
-	 * 
-	 * @param length
-	 * @return
+	 * Calculates the cross product of this vector and given vector and returns 
+	 * the resulting vector.
+	 * @param other the right-hand side vector for the cross product
+	 * @return the resulting vector
 	 */
-	public Vector3D toLength(float length) {
-		return this.scalarDivision(getLength()).scalarMultiplication(length);
-	}
-	
 	public Vector3D crossProduct(Vector3D other) {
 		return new Vector3D(
 				(y*other.z - z*other.y),
@@ -86,14 +103,35 @@ public class Vector3D {
 				(x*other.y - y*other.x));
 	}
 	
+	/**
+	 * Calculates the dot product of this vector and given vector and returns 
+	 * the resulting scalar.
+	 * @param other the right-hand side vector for the dot product
+	 * @return the resulting scalar
+	 */
 	public float dotProduct(Vector3D other) {
 		return (x * other.x) + (y * other.y) + (z * other.z);
 	}
 	
+	/**
+	 * Returns a vector that has the same direction as this, but magnitude set 
+	 * to a given scalar.
+	 * @param length the length of the resulting vector
+	 * @return the resulting vector
+	 */
+	public Vector3D toLength(float length) {
+		return this.normalize().scalarMultiplication(length);
+	}
+	
+	/**
+	 * Returns a vector that has the same magnitude as this but points towards 
+	 * given position.
+	 * @param position the position the resulting vector should point towards
+	 * @return the resulting vector
+	 */
 	public Vector3D directedTowards(Vector3D position) {
-		if(x == position.x && y == position.y && z == position.z) {
-			return this;
-		}
+		Vector3D directed = position.subtract(this);
+		if(directed.length <= 0) return new Vector3D(0,0,0);
 		
 		return position.subtract(this).toLength(this.getLength());
 	}
