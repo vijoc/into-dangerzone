585b13b1352b5f7eb5fa43d9e5360429dc9dabc6
Ville Kukkonen <kukkonen.ville@gmail.com>
2014-10-26 18:34:01 +0200

    Implement Scene abstract class

---
+++ IntoDangerzone/src/core/Scene.java
@@ -0,0 +1,27 @@
+package core;
+
+import processing.core.PApplet;
+
+/**
+ * A scene is the unit of presentation.
+ */
+public abstract class Scene {
+	
+	protected PApplet parent;
+	
+	public Scene(PApplet parent) {
+		this.parent = parent;
+	}
+	
+	/** 
+	 * Forward the internal state of the scene by given value.
+	 * @param dtSeconds seconds since last call to update.
+	 */
+	public abstract void update(float dtSeconds);
+	
+	/**
+	 * Render the state of the scene.
+	 */
+	public abstract void render();
+	
+}
