6b85c4bfc5629da6d8da632d2021b9470e2b4e43
Jukka Eerikäinen <jukka.eerikainen@aalto.fi>
2014-10-29 00:01:30 +0200

    add some more NICE RULESETS ey

--- IntoDangerzone/src/scenes/gameoflife/GameOfLife.java
+++ IntoDangerzone/src/scenes/gameoflife/GameOfLife.java
@@ -3,55 +3,60 @@ package scenes.gameoflife;
 import java.util.Random;
 
 class GameOfLife {
-	
+
 	/** Default birth rules */
 	public static final int[] DEFAULT_BIRTH_COUNTS = { 3 };
-	
+
 	/** Default survival rules */
 	public static final int[] DEFAULT_SURVIVAL_COUNTS = { 2, 3 };
-	
-	private RuleSet ruleSet = new RuleSet(DEFAULT_BIRTH_COUNTS, DEFAULT_SURVIVAL_COUNTS);
-	
+
+	private RuleSet ruleSet = new RuleSet(DEFAULT_BIRTH_COUNTS,
+			DEFAULT_SURVIVAL_COUNTS);
+
 	private int rowCount;
 	private int colCount;
 	private int generation;
-	
+
 	private float density = 0.0f;
-	
+
 	private final int maxRowIndex;
 	private final int maxColIndex;
-	
+
 	/** Cell states as boolean array, indexed by x,y so first index is column */
 	private boolean states[][];
-	
+
 	/**
 	 * Construct a new GameOfLife with given dimensions.
-	 * @param colCount number of columns, i.e. the x dimension
-	 * @param rowCount number of rows, i.e. the y dimension
+	 * 
+	 * @param colCount
+	 *            number of columns, i.e. the x dimension
+	 * @param rowCount
+	 *            number of rows, i.e. the y dimension
 	 */
 	public GameOfLife(int colCount, int rowCount) {
 		this.colCount = colCount;
 		this.rowCount = rowCount;
-		
-		maxRowIndex = rowCount-1;
-		maxColIndex = colCount-1;
-		
+
+		maxRowIndex = rowCount - 1;
+		maxColIndex = colCount - 1;
+
 		states = new boolean[colCount][rowCount];
 		generation = 0;
 	}
-	
+
 	/**
 	 * Seed a random state.
 	 */
 	public void seedRandom() {
 		Random rand = new Random();
-		for(int i = 0; i < colCount; i++) {
-			for(int j = 0; j < rowCount; j++) {
-				if(rand.nextFloat() < density) setCellState(i, j, true);
+		for (int i = 0; i < colCount; i++) {
+			for (int j = 0; j < rowCount; j++) {
+				if (rand.nextFloat() < density)
+					setCellState(i, j, true);
 			}
 		}
 	}
-	
+
 	/**
 	 * Step forward a generation.
 	 */
@@ -59,37 +64,47 @@ class GameOfLife {
 		states = calculateNextStates();
 		generation++;
 	}
-	
+
 	/**
 	 * Get the state of the cell at coordinates (x,y).
-	 * @param x column coordinate
-	 * @param y row coordinate
+	 * 
+	 * @param x
+	 *            column coordinate
+	 * @param y
+	 *            row coordinate
 	 * @return
 	 */
 	public boolean getCellState(int x, int y) {
 		return states[wrapColumn(x)][wrapRow(y)];
 	}
-	
+
 	/**
 	 * Set the state of the cell at coordinates (x,y).
-	 * @param x column coordinate
-	 * @param y row coordinate
-	 * @param state new state
+	 * 
+	 * @param x
+	 *            column coordinate
+	 * @param y
+	 *            row coordinate
+	 * @param state
+	 *            new state
 	 */
 	public void setCellState(int x, int y, boolean state) {
 		states[wrapColumn(x)][wrapRow(y)] = state;
 	}
-	
+
 	/**
 	 * Insert a shape specified by boolean[x][y] array
+	 * 
 	 * @param shape
-	 * @param x leftmost x coordinate
-	 * @param y upmost y coordinate
+	 * @param x
+	 *            leftmost x coordinate
+	 * @param y
+	 *            upmost y coordinate
 	 */
-	public void insertShape(boolean[][] shape, int x, int y){
-		for (int i = 0; i < shape.length; i++){
-			for (int j = 0; j < shape[0].length; j++){
-				setCellState(x+i, y+j, shape[i][j]);
+	public void insertShape(boolean[][] shape, int x, int y) {
+		for (int i = 0; i < shape.length; i++) {
+			for (int j = 0; j < shape[0].length; j++) {
+				setCellState(x + i, y + j, shape[i][j]);
 			}
 		}
 	}
@@ -97,76 +112,84 @@ class GameOfLife {
 	public int getColumnCount() {
 		return colCount;
 	}
-	
+
 	public int getRowCount() {
 		return rowCount;
 	}
-	
+
 	public RuleSet getRuleSet() {
 		return ruleSet;
 	}
-	
+
+	public void setRuleSet(RuleSet ruleSet) {
+		this.ruleSet = ruleSet;
+	}
+
 	private boolean[][] calculateNextStates() {
 		boolean buffer[][] = new boolean[colCount][rowCount];
-		for(int x = 0; x < colCount; x++) {
-			for(int y = 0; y < rowCount; y++) {
+		for (int x = 0; x < colCount; x++) {
+			for (int y = 0; y < rowCount; y++) {
 				buffer[x][y] = calculateNextState(x, y);
 			}
 		}
 		return buffer;
 	}
-	
+
 	private boolean calculateNextState(int x, int y) {
-		int neighbourCount = countAliveNeighbours(x,y);
-		
-		if(getCellState(x, y) == true) {
+		int neighbourCount = countAliveNeighbours(x, y);
+
+		if (getCellState(x, y) == true) {
 			return ruleSet.survivalRulesContain(neighbourCount);
 		} else {
 			return ruleSet.birthRulesContain(neighbourCount);
 		}
 	}
-	
+
 	private int countAliveNeighbours(int x, int y) {
 		int sum = 0;
-		
+
 		int col, row;
-		for(int i = -1; i <= 1; i++) {
+		for (int i = -1; i <= 1; i++) {
 			col = wrapColumn(x + i);
-			
-			for(int j = -1; j <= 1; j++) {
-				if(i == 0 && j == 0) continue; // skip self
-				
+
+			for (int j = -1; j <= 1; j++) {
+				if (i == 0 && j == 0)
+					continue; // skip self
+
 				row = wrapRow(y + j);
-				
-				if(getCellState(col, row)) sum += 1;
+
+				if (getCellState(col, row))
+					sum += 1;
 			}
 		}
-		
+
 		return sum;
 	}
-	
+
 	private int wrapColumn(int x) {
 		return wrapAround(x, maxColIndex);
 	}
-	
+
 	private int wrapRow(int y) {
 		return wrapAround(y, maxRowIndex);
 	}
-	
+
 	/**
-	 * A helper to wrap the coordinates around. Given a number and a maximum, wraps 
-	 * the number in the range [0..maximum] inclusive.
+	 * A helper to wrap the coordinates around. Given a number and a maximum,
+	 * wraps the number in the range [0..maximum] inclusive.
+	 * 
 	 * @param number
 	 * @param max
 	 * @return
 	 */
 	private static int wrapAround(int number, int max) {
-		if(number >= 0 && number <= max) // within bounds, return self
+		if (number >= 0 && number <= max) // within bounds, return self
 			return number;
-		else if(number < 0) // less than zero, wrap around
+		else if (number < 0) // less than zero, wrap around
 			return max + number;
-		else // greater than max, wrap around
-			return number - max;	
+		else
+			// greater than max, wrap around
+			return number - max;
 	}
 
 	public int getGeneration() {
--- IntoDangerzone/src/scenes/gameoflife/RuleSet.java
+++ IntoDangerzone/src/scenes/gameoflife/RuleSet.java
@@ -3,19 +3,20 @@ package scenes.gameoflife;
 import java.util.Arrays;
 
 public class RuleSet {
-	
+
 	private int[] birthCounts = {};
 	private int[] survivalCounts = {};
-	
+
 	/**
 	 * Construct a default, empty RuleSet.
 	 */
 	public RuleSet() {
-		
+
 	}
-	
+
 	/**
 	 * Construct a RuleSet with given birth rules and survival rules.
+	 * 
 	 * @param birthCounts
 	 * @param survivalCounts
 	 */
@@ -23,20 +24,20 @@ public class RuleSet {
 		setBirthRules(birthCounts);
 		setSurvivalRules(survivalCounts);
 	}
-	
+
 	public boolean survivalRulesContain(int count) {
 		return Arrays.binarySearch(survivalCounts, count) >= 0;
 	}
-	
+
 	public boolean birthRulesContain(int count) {
 		return Arrays.binarySearch(birthCounts, count) >= 0;
 	}
-	
+
 	public void setBirthRules(int... counts) {
 		this.birthCounts = counts;
 		Arrays.sort(birthCounts);
 	}
-	
+
 	public void setSurvivalRules(int... counts) {
 		this.survivalCounts = counts;
 		Arrays.sort(survivalCounts);
