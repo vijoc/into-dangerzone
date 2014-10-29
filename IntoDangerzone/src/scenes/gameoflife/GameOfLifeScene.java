af9e9fd84bbccc132ec5dfed5a00b059dbf47f8f
Jukka Eerikäinen <jukka.eerikainen@aalto.fi>
2014-10-29 11:32:01 +0200

    move known good rule sets under RuleSet

--- IntoDangerzone/src/scenes/gameoflife/GameOfLifeScene.java
+++ IntoDangerzone/src/scenes/gameoflife/GameOfLifeScene.java
@@ -1,6 +1,5 @@
 package scenes.gameoflife;
 
-import java.util.ArrayList;
 import java.util.Random;
 
 import audio.AudioAnalyser;
@@ -26,42 +25,13 @@ public class GameOfLifeScene extends Scene {
 
 	private AudioAnalyser audioAnalyser;
 	private BeatListener beatListener;
-	
-	/**
-	 * Known good rule sets TODO I am a bit unsure where they actually belong
-	 */
-	private static final RuleSet AMOEBA = new RuleSet(new int[] { 3, 5, 7 },
-			new int[] { 1, 3, 5, 8 });
-	private static final RuleSet ASSIMILATION = new RuleSet(
-			new int[] { 3, 4, 5 }, new int[] { 4, 5, 6, 7 });
-	private static final RuleSet CORAL = new RuleSet(new int[] { 3 },
-			new int[] { 4, 5, 6, 7, 8 });
-	private static final RuleSet DAY_AND_NIGHT = new RuleSet(new int[] { 3, 6,
-			7, 8 }, new int[] { 3, 4, 6, 7, 8 });
-	private static final RuleSet GNARL = new RuleSet(new int[] { 1 },
-			new int[] { 1 });
-	private static final RuleSet GOL = new RuleSet(new int[] { 3 }, new int[] {
-			2, 3 });
-	private static final RuleSet LIFE_WITHOUT_DEATH = new RuleSet(
-			new int[] { 3 }, new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8 });
-	private static final RuleSet MAZE = new RuleSet(new int[] { 3 }, new int[] {
-			1, 2, 3, 4, 5 });
-	private static final RuleSet MAZECTRIC = new RuleSet(new int[] { 3 },
-			new int[] { 1, 2, 3, 4 });
-	private static final RuleSet PSEUDO_LIFE = new RuleSet(
-			new int[] { 3, 5, 7 }, new int[] { 2, 3, 8 });
-	private static final RuleSet REPLICATOR = new RuleSet(new int[] { 1, 3, 5,
-			7 }, new int[] { 1, 3, 5, 7 });
-	private static final RuleSet STAINS = new RuleSet(new int[] { 3, 6, 7, 8 },
-			new int[] { 2, 3, 5, 6, 7, 8 });
-	private static final RuleSet TWO_X_TWO = new RuleSet(new int[] { 3, 6 },
-			new int[] { 1, 2, 5 });
-	private static final RuleSet WALLED_CITIES = new RuleSet(new int[] { 4, 5,
-			6, 7, 8 }, new int[] { 2, 3, 4, 5 });
-	public static final RuleSet[] RULE_SETS = { AMOEBA, ASSIMILATION, CORAL,
-			DAY_AND_NIGHT, GNARL, GOL, LIFE_WITHOUT_DEATH, MAZE, MAZECTRIC,
-			PSEUDO_LIFE, REPLICATOR, STAINS, TWO_X_TWO, WALLED_CITIES };
 
+	public static final RuleSet[] RULE_SETS = { RuleSet.AMOEBA,
+			RuleSet.ASSIMILATION, RuleSet.CORAL, RuleSet.DAY_AND_NIGHT,
+			RuleSet.GNARL, RuleSet.GOL, RuleSet.LIFE_WITHOUT_DEATH,
+			RuleSet.MAZE, RuleSet.MAZECTRIC, RuleSet.PSEUDO_LIFE,
+			RuleSet.REPLICATOR, RuleSet.STAINS, RuleSet.TWO_X_TWO,
+			RuleSet.WALLED_CITIES };
 
 	public GameOfLifeScene(PApplet parent, AudioSource audioSource,
 			float stepDuration, int columns, int rows) {
