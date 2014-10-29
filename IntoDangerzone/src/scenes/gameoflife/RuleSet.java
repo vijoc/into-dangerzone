96a275ad02f59eb039efef8c5bcb8852d760979f
Jukka Eerikäinen <jukka.eerikainen@aalto.fi>
2014-10-29 11:48:26 +0200

    Add good rulesets

--- IntoDangerzone/src/scenes/gameoflife/RuleSet.java
+++ IntoDangerzone/src/scenes/gameoflife/RuleSet.java
@@ -11,26 +11,42 @@ public class RuleSet {
 			new int[] { 1, 3, 5, 8 });
 	public static final RuleSet ASSIMILATION = new RuleSet(
 			new int[] { 3, 4, 5 }, new int[] { 4, 5, 6, 7 });
+	public static final RuleSet COAGULATIONS = new RuleSet(
+			new int[] { 3, 7, 8 }, new int[] { 2, 3, 5, 6, 7, 8 });
 	public static final RuleSet CORAL = new RuleSet(new int[] { 3 }, new int[] {
 			4, 5, 6, 7, 8 });
 	public static final RuleSet DAY_AND_NIGHT = new RuleSet(new int[] { 3, 6,
 			7, 8 }, new int[] { 3, 4, 6, 7, 8 });
+	public static final RuleSet DIAMOEBA = new RuleSet(new int[] { 3, 5, 6, 7,
+			8 }, new int[] { 5, 6, 7, 8 });
 	public static final RuleSet GNARL = new RuleSet(new int[] { 1 },
 			new int[] { 1 });
 	public static final RuleSet GOL = new RuleSet(new int[] { 3 }, new int[] {
 			2, 3 });
+	public static final RuleSet HIGH_LIFE = new RuleSet(new int[] { 3, 6 },
+			new int[] { 2, 3 });
 	public static final RuleSet LIFE_WITHOUT_DEATH = new RuleSet(
 			new int[] { 3 }, new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8 });
+	public static final RuleSet LONG_LIFE = new RuleSet(new int[] { 3, 4, 5 },
+			new int[] { 5 });
 	public static final RuleSet MAZE = new RuleSet(new int[] { 3 }, new int[] {
 			1, 2, 3, 4, 5 });
 	public static final RuleSet MAZECTRIC = new RuleSet(new int[] { 3 },
 			new int[] { 1, 2, 3, 4 });
+	public static final RuleSet MOVE = new RuleSet(new int[] { 3, 6, 8 },
+			new int[] { 2, 4, 5 });
 	public static final RuleSet PSEUDO_LIFE = new RuleSet(
 			new int[] { 3, 5, 7 }, new int[] { 2, 3, 8 });
 	public static final RuleSet REPLICATOR = new RuleSet(
 			new int[] { 1, 3, 5, 7 }, new int[] { 1, 3, 5, 7 });
+	public static final RuleSet SEEDS_TWO = new RuleSet(new int[] { 2 },
+			new int[] {});
+	public static final RuleSet SERVIETTES = new RuleSet(new int[] { 2, 3, 4 },
+			new int[] {});
 	public static final RuleSet STAINS = new RuleSet(new int[] { 3, 6, 7, 8 },
 			new int[] { 2, 3, 5, 6, 7, 8 });
+	public static final RuleSet THREE_FOUR_LIFE = new RuleSet(
+			new int[] { 3, 4 }, new int[] { 3, 4 });
 	public static final RuleSet TWO_X_TWO = new RuleSet(new int[] { 3, 6 },
 			new int[] { 1, 2, 5 });
 	public static final RuleSet WALLED_CITIES = new RuleSet(new int[] { 4, 5,
