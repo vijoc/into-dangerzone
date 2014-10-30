5d60c13e97074036b14aa7867e1d79d2e7395e88
Jukka Eerikäinen <jukka.eerikainen@aalto.fi>
2014-10-30 11:07:55 +0200

    Add another rendering mode, add keyboard controls. SPACE changes rendering mode, U/D arrows change X curl target, L/R arrows change Y curl target

--- IntoDangerzone/src/scenes/lTree/LTree.java
+++ IntoDangerzone/src/scenes/lTree/LTree.java
@@ -1,5 +1,8 @@
 package scenes.lTree;
 
+import java.awt.KeyEventDispatcher;
+import java.awt.KeyboardFocusManager;
+import java.awt.event.KeyEvent;
 import java.util.Random;
 
 import math.Vector3D;
@@ -8,7 +11,12 @@ import ddf.minim.AudioSource;
 import audio.BeatListener;
 import processing.core.PApplet;
 
-public class LTree extends core.Scene {
+public class LTree extends core.Scene implements KeyEventDispatcher {
+
+	private enum RenderingMode {
+		LINES, POINTS
+	};
+
 	AudioSource audioSource;
 	BeatListener beatListener;
 
@@ -17,14 +25,23 @@ public class LTree extends core.Scene {
 	float curlX = 0;
 	float curlY = 0;
 	int branchNumber = 15;
-	float f = 0;
-	float fTarget = 0.75f;
+
+	float branchRatio = 0;
+	float branchRatioTarget = 0.75f;
+
 	float delay = 50;
+
 	float growth = 0.15f;
 	float growthTarget = 1.1f;
+
 	float curlXTarget = 0;
 	float curlYTarget = 0;
+
+	float chanceOfChangeFromLines = 0.1f;
+	float chanceOfChangeFromPoints = 0.4f;
 	Random rand;
+
+	RenderingMode renderingMode;
 
 	public LTree(PApplet parent, AudioSource audioSource) {
 		super(parent);
@@ -32,19 +49,22 @@ public class LTree extends core.Scene {
 		this.beatListener = new BeatListener(audioSource);
 		this.camera = new Camera(parent);
 		this.rand = new Random();
+		this.renderingMode = RenderingMode.LINES;
 	}
 
 	@Override
 	public void update(float dtSeconds) {
 		if (beatListener.isKick()) {
-			curlXTarget += rand.nextFloat() * 5 - 10;
+			curlYTarget += rand.nextFloat() * 50 - 25;
+			if (rand.nextFloat() < getRenderingChangeChance())
+				changeRenderingMode();
 		}
 		if (beatListener.isSnare()) {
-			curlYTarget += rand.nextFloat() * 5 - 10;
+			curlXTarget += rand.nextFloat() * 50 - 25;
 		}
 		if (beatListener.isHat()) {
-			if (f < fTarget)
-				f += 0.00025;
+			if (branchRatio < branchRatioTarget)
+				branchRatio += 0.00025;
 			if (growth < growthTarget)
 				growth += 0.0005;
 		}
@@ -52,40 +72,132 @@ public class LTree extends core.Scene {
 
 	@Override
 	public void render() {
+		clearBackground();
 		parent.translate(0, 0);
-
-		parent.background(250, 250, 250);
 		parent.stroke(0, 0, 0, 50);
 		curlX += (PApplet.radians((float) (curlXTarget)) - curlX) / delay;
 		curlY += (PApplet.radians((float) (curlYTarget)) - curlY) / delay;
 		branch(parent.height / 3, branchNumber);
 	}
 
-	private void branch(double d, int num) {
-		d *= f;
-		num -= 1;
-		if ((d > 1) && (num > 0)) {
+	private void branch(double length, int branchNum) {
+		length *= branchRatio;
+		branchNum -= 1;
+		if ((length > 1) && (branchNum > 0)) {
 			parent.pushMatrix();
 			parent.rotate(curlX);
-			parent.line(0, 0, 0, (float) -d);
-			parent.translate(0, (float) -d);
-			branch(d, num);
+			renderBranch(length);
+
+			parent.translate(0, (float) -length);
+			branch(length, branchNum);
 			parent.popMatrix();
 
-			d *= growth;
+			length *= growth;
 			parent.pushMatrix();
 			parent.rotate(curlX - curlY);
-			parent.line(0, 0, 0, (float) -d);
-			parent.translate(0, (float) -d);
-			branch(d, num);
+			renderBranch(length);
+
+			parent.translate(0, (float) -length);
+			branch(length, branchNum);
 			parent.popMatrix();
 		}
+	}
 
+	private void clearBackground() {
+		switch (renderingMode) {
+		case POINTS:
+			parent.fill(255, 10);
+			parent.noStroke();
+			parent.rectMode(PApplet.RADIUS);
+			parent.rect(0, 0, parent.width, parent.height);
+			parent.rectMode(PApplet.CORNER);
+			break;
+		case LINES:
+			parent.background(255, 255, 255);
+			break;
+		default:
+			break;
+		}
+	}
+
+	private void renderBranch(double length) {
+		switch (renderingMode) {
+		case POINTS:
+			parent.point(0, 0);
+			break;
+		case LINES:
+			parent.line(0, 0, 0, (float) -length);
+			break;
+		default:
+			break;
+		}
+	}
+
+	public void changeRenderingMode() {
+		switch (renderingMode) {
+		case LINES:
+			renderingMode = RenderingMode.POINTS;
+			break;
+		case POINTS:
+			renderingMode = RenderingMode.LINES;
+			break;
+		default:
+			break;
+		}
+	}
+
+	private float getRenderingChangeChance() {
+		float result = 1.0f;
+		switch (renderingMode) {
+		case LINES:
+			result = chanceOfChangeFromLines;
+			break;
+		case POINTS:
+			result = chanceOfChangeFromPoints;
+			break;
+		default:
+			break;
+		}
+		return result;
+	}
+
+	@Override
+	public boolean dispatchKeyEvent(KeyEvent e) {
+		switch (e.getID()) {
+		case KeyEvent.KEY_PRESSED:
+			keyPress(e.getKeyCode());
+			break;
+		}
+		return false;
+	}
+
+	private void keyPress(int code) {
+		switch (code) {
+		case KeyEvent.VK_SPACE:
+			changeRenderingMode();
+			break;
+		case KeyEvent.VK_D:
+			break;
+		case KeyEvent.VK_UP:
+			curlXTarget += 10;
+			break;
+		case KeyEvent.VK_DOWN:
+			curlXTarget -= 10;
+			break;
+		case KeyEvent.VK_RIGHT:
+			curlYTarget += 10;
+			break;
+		case KeyEvent.VK_LEFT:
+			curlYTarget -= 10;
+			break;
+		}
 	}
 
 	@Override
 	public void activated() {
 		updateCamera();
+		KeyboardFocusManager.getCurrentKeyboardFocusManager()
+				.addKeyEventDispatcher(this);
 	}
 
 	private void updateCamera() {
@@ -102,7 +214,8 @@ public class LTree extends core.Scene {
 
 	@Override
 	public void deactivated() {
-		// TODO Auto-generated method stub
+		KeyboardFocusManager.getCurrentKeyboardFocusManager()
+				.removeKeyEventDispatcher(this);
 
 	}
 }
