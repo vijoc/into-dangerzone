0baa4e07c4262da37995b1b8c5ad0920d821086e
Jukka Eerikäinen <jukka.eerikainen@aalto.fi>
2015-02-02 00:20:06 +0200

    fokken' unknown error

--- IntoDangerzone/src/data/saffrag.glsl
+++ IntoDangerzone/src/data/saffrag.glsl
@@ -4,11 +4,11 @@ precision mediump int;
 #endif
 
 varying vec4 vertColor;
-uniform float fft[];
-uniform float waveform[];
+uniform float fft[1024];
+uniform float waveform[1024];
 
 void main() {
   int posX = int(gl_FragCoord.x);
   int posY = int(gl_FragCoord.y);
-  gl_FragColor = vec4(waveform.get(posX), fft[posY], 1, 1);
+  gl_FragColor = vec4(waveform[posX], fft[0], 1, 1);
 }
